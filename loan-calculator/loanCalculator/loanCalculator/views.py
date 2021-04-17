ac75a00b5a3876060b3b74db7fc80e169ce96540
shilpa <shingnapureshilpa2@gmail.com>
2021-04-17 11:22:22 +0530

    changes in loan Calculator project

--- loan-calculator/loanCalculator/loanCalculator/views.py
+++ loan-calculator/loanCalculator/loanCalculator/views.py
@@ -11,11 +11,17 @@ def home(request):
         month = request.POST['month']
     
     monthly = 0
+    total_months = 0
+    extra_money = 0
     if amount != '' and rate != '' and month != '':
         amount = int(amount)
         rate = float(rate)
         month = int(month)
         interset = (amount *(rate * .01))/month
         monthly = round(((amount / month ) + interset),2)
+        total_months = monthly * month
+        extra_money = total_months - amount
     monthly = f'$ {monthly}'
-    return render(request,'loan.html',{'ans':monthly})
+    total_months = f'$ {total_months}'
+    extra_money = f'$ {extra_money}'
+    return render(request,'loan.html',{'ans':monthly,'total_months':total_months,'extra_money':extra_money})
